917d7a608c6eccd359adcb418e98f41663df4ca6
Lu <1300743936@qq.com>
2016-10-21 18:56:26 +0800

    修改注册

--- WebContent/WebApp/js/index.js
+++ WebContent/WebApp/js/index.js
@@ -1,12 +1,61 @@
 $(function(){
 	$('.close').on('click', function() {
 		$('.hidden').css({
+			"visibility":"hidden"
+		});
+		$('.box').css({
+			"width":"0",
+			"height":"0"
+		});
+	});
+	$('.login-button').on('click', function() {
+		$('.hidden').css({
+			"visibility":"visible"
+		});
+		$('.box').animate({
+			"width":"3.9rem",
+			"height":"3.1rem"
+		},"swing");
+		$('.box-hotel-register').css({
+			"font-size":".16rem",
+			"color":"#7f1f59",
+			"margin-top":".24rem"
+		});
+		$('.box-hotel-login').css({
+			"font-size":".2rem",
+			"color":"#000",
+			"margin-top":".21rem"
+		});
+		$('.login-box').css({
+			"display":"block"
+		});
+		$('.register-box').css({
 			"display":"none"
 		});
 	});
-	$('.login').on('click', function() {
+	$('.register-button').on('click', function() {
 		$('.hidden').css({
+			"visibility":"visible"
+		});
+		$('.box').animate({
+			"width":"3.9rem",
+			"height":"3.7rem"
+		},"swing");
+		$('.box-hotel-login').css({
+			"font-size":".16rem",
+			"color":"#7f1f59",
+			"margin-top":".24rem"
+		});
+		$('.box-hotel-register').css({
+			"font-size":".2rem",
+			"color":"#000",
+			"margin-top":".21rem"
+		});
+		$('.register-box').css({
 			"display":"block"
+		});
+		$('.login-box').css({
+			"display":"none"
 		});
 	});
 	$('#box_up').on('click',  function() {
@@ -18,12 +67,79 @@ $(function(){
    			"dataType":"json",
    			"data":{"username":$username,"password":$password},
    			"success":function(data){
-   				alert(data.status);
+   				if(data.status=="1"){
+   					setTimeout(function(){
+   						window.location.href = "info.html";
+   					},500)
+   				}
    			},
    			"error":function(){
-   				alert("请稍后再试！");
+   				alert("网络不畅，请稍后再试！");
    			}
-
 		})
+	});
+	$('#box_up_r').on('click',  function() {
+		var $username = $("#register_username").val();
+		var $password = $("#register_password").val();
+		$.ajax({
+   			"type":"GET",
+   			"url":"/hm/register.action",
+   			"dataType":"json",
+   			"data":{"username":$username,"password":$password},
+   			"success":function(data){
+   				alert("success!");
+   			},
+   			"error":function(){
+   				alert("网络不畅，请稍后再试！");
+   			}
+		})
+	});
+	$('#register_username').on('blur',  function() {
+		event.preventDefault();
+		console.log($(this))
+		if($(this).val()==""){
+			$('.error-username').text('用户名不能为空');
+		}
+		else{
+			$('.error-username').text('');
+		}
+	});
+	$('.bar').on('mouseover', 'li', function() {
+		event.preventDefault();
+		$('.bar li').removeClass('active');
+		var $value = $(this).attr("value");
+		if ($value=="2") {
+			$(this).addClass('active');
+			$("#bar_LP").css({
+				"left":"2.96rem",
+				"width":"0.72rem"
+			});
+		}
+		else if($value==3){
+			$(this).addClass('active');
+			$("#bar_LP").css({
+				"left":"5.48rem",
+				"width":"0.72rem"
+			});
+		}
+		else if($value==1){
+			$(this).addClass('active');
+			$("#bar_LP").css({
+				"left":".8rem",
+				"width":"0.36rem"
+			});
+		}
+	});
+	$('.bar').on('mouseout', 'li', function() {
+		event.preventDefault();
+		var $value = $(this).attr("value");
+		if ($value!="1") {
+			$(this).removeClass('active');
+			$("#bar_first").addClass('active');
+		}
+		$("#bar_LP").css({
+				"left":".8rem",
+				"width":"0.36rem"
+			});
 	});
 })
--- src/action/user/UserRegisterAction.java
+++ src/action/user/UserRegisterAction.java
@@ -15,7 +15,7 @@ import com.opensymphony.xwork2.ActionSupport;
 import entity.User;
 import service.UserService;
 
-public class UserAction extends ActionSupport {
+public class UserRegisterAction extends ActionSupport {
 
 	private String username;
 	private String password;
@@ -58,16 +58,19 @@ public class UserAction extends ActionSupport {
 		JSON_Object = new JSONObject();
 		int status;
 		User u = new User();
+		User user = new User();
+		user.setUsername(username);
+		user.setPassword(password);
+		user.setId(3);
 		Iterator<User> it = list.iterator();
 		PrintWriter out = response.getWriter();
-		System.out.println(username);
-
+		
 		while (it.hasNext()) {
 			
 			u = it.next();
-			if (u.getUsername().equals(username) && u.getPassword().equals(password)) {
+			if (u.getUsername().equals(username)) {
 				
-				status=1;
+				status=0;
 				JSON_Object.put("status", status);
 				out.write(JSON_Object.toString());
 				out.close();
@@ -76,8 +79,11 @@ public class UserAction extends ActionSupport {
 
 			
 		}
+		System.out.println(user.getUsername());
+		userService.save(user);
 		
-		status = 0;
+		status = 1;
+		System.out.println(password);
 		JSON_Object.put("status", status);
 		out.write(JSON_Object.toString());
 		out.close();
