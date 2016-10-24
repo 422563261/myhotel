c43d7f39045e1aeacba5da0b684dad7b86f5094c
Ricky Tan <422563261@qq.com>
2016-10-24 16:53:48 +0800

    js文件分块

--- WebContent/WebApp/js/about.js
+++ WebContent/WebApp/js/about.js
@@ -5,7 +5,9 @@ $(function(){
 		dataType: 'html',
 		success:function(data){
 			$('body').append(data);
+			$('.info').remove();
 			$('.hidden').before("<script src='js/main.js'><\/script>");
 		}
 	});
+	
 })
