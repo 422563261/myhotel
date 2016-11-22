c397871fb9fb638ded0dd5f5f5648266c2e6934a
Ricky Tan <422563261@qq.com>
2016-11-22 18:41:23 +0800

    bug

--- WebContent/WebApp/js/My-hotel.js
+++ WebContent/WebApp/js/My-hotel.js
@@ -17,6 +17,53 @@ $(function(){
 					if(data[0].status=="1"){
 						$username = data[1].username;
 						$money = data[1].money;
+						$.ajax({
+							url: '/Hotel/WebApp/My-body.html',
+							type: 'GET',
+							dataType: 'html',
+							data: {},
+							success:function(data){
+								$('body').append(data);
+								$("#money").text($money);
+								$('.username').text($username);
+								console.log($username);
+								$('.state').on('click', 'li', function(event) {
+									event.preventDefault();
+									$(this).css({
+										"color": "#651c4d",
+										"border-top": ".02rem solid #d987bb",
+										"margin-bottom": "-.01rem",
+										"background": "#fff",
+										"line-height": ".39rem"
+									}).siblings().css({
+										"color": "#000",
+										"border-top": ".01rem solid #eeeae9",
+										"margin-bottom": "0rem",
+										"background": "#fcfafa",
+										"line-height":".41rem"
+									})
+								});
+								$.ajax({
+									url: '/Hotel/WebApp/getOrder.action',
+									type: 'GET',
+									dataType: 'json',
+									data: {},
+									success:function(data){
+										var data =  $.parseJSON(data.content);
+										var $startDay = (data[0].startDay).slice(0,4)+"/"+(data[0].startDay).slice(5,7)+"/"+(data[0].startDay).slice(8,10);
+										var $finalDay = (data[0].finalDay).slice(0,4)+"/"+(data[0].finalDay).slice(5,7)+"/"+(data[0].finalDay).slice(8,10);
+										$("#orderId").text(data[0].orderId);
+										$("#roomId").text(data[0].roomId);
+										$("#name").text(data[0].name);
+										$("#totalMoney").text("¥" + data[0].totalMoney);
+										$("#status").text(data[0].Status);
+										$("#bookDay").text(data[0].bookDay);
+										$("#startDay").text($startDay);
+										$("#finalDay").text($finalDay);
+									}
+								})
+							}
+						})
 					}
 					else{
 						alert("你还未登录！");
@@ -27,6 +74,7 @@ $(function(){
 					$('#username').text("NULL");
 				}
 			});
+
 			$("#logout").on('click', function(event) {
 				event.preventDefault();
 				$.ajax({
@@ -47,54 +95,6 @@ $(function(){
 				event.preventDefault();
 				location.href="/Hotel/WebApp/My-hotel.html"
 			});
-		},
-		complete:function(){
-			$.ajax({
-				url: '/Hotel/WebApp/My-body.html',
-				type: 'GET',
-				dataType: 'html',
-				data: {},
-				success:function(data){
-					$('body').append(data);
-					$("#money").text($money);
-					$('.username').text($username);
-					$('.state').on('click', 'li', function(event) {
-						event.preventDefault();
-						$(this).css({
-							"color": "#651c4d",
-							"border-top": ".02rem solid #d987bb",
-							"margin-bottom": "-.01rem",
-							"background": "#fff",
-							"line-height": ".39rem"
-						}).siblings().css({
-							"color": "#000",
-							"border-top": ".01rem solid #eeeae9",
-							"margin-bottom": "0rem",
-							"background": "#fcfafa",
-							"line-height":".41rem"
-						})
-					});
-					$.ajax({
-						url: '/Hotel/WebApp/getOrder.action',
-						type: 'GET',
-						dataType: 'json',
-						data: {},
-						success:function(data){
-							var data =  $.parseJSON(data.content);
-							var $startDay = (data[0].startDay).slice(0,4)+"/"+(data[0].startDay).slice(5,7)+"/"+(data[0].startDay).slice(8,10);
-							var $finalDay = (data[0].finalDay).slice(0,4)+"/"+(data[0].finalDay).slice(5,7)+"/"+(data[0].finalDay).slice(8,10);
-							$("#orderId").text(data[0].orderId);
-							$("#roomId").text(data[0].roomId);
-							$("#name").text(data[0].name);
-							$("#totalMoney").text("¥" + data[0].totalMoney);
-							$("#status").text(data[0].Status);
-							$("#bookDay").text(data[0].bookDay);
-							$("#startDay").text($startDay);
-							$("#finalDay").text($finalDay);
-						}
-					})
-				}
-			})
 		}
-	})
+	});
 })
