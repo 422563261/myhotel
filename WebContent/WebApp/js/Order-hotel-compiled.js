ed79b204850bf253c973042da6f0608fada8d92c
Ricky Tan <422563261@qq.com>
2016-11-30 19:49:55 +0800

    订单页面

--- WebContent/WebApp/js/Order-hotel-compiled.js
+++ WebContent/WebApp/js/Order-hotel-compiled.js
@@ -1,15 +1,15 @@
 /**
  * Created by tan on 2016/11/18.
  */
-define(['jquery','jqueryForm','login'],function($){
+define(['jquery', 'jqueryForm', 'login'], function ($) {
     return {
-        orderHotel:function(){
-            $(function(){
+        orderHotel: function () {
+            $(function () {
                 var $register_username;
                 var $register_password;
                 var $register_password_confirm;
-                var $reg1 = /^[A-Za-z0-9]{3,11}$/;//用户名要求是3~11的位的字母或者数字
-                var $reg2 = /^[A-Za-z0-9]{6,11}$/;//密码要求是6~11的位的字母或者数字
+                var $reg1 = /^[A-Za-z0-9]{3,11}$/; //用户名要求是3~11的位的字母或者数字
+                var $reg2 = /^[A-Za-z0-9]{6,11}$/; //密码要求是6~11的位的字母或者数字
                 var model = `<div class="hotel">
                                 <div class="pic">
                                     <img src="images/hotel.jpg" alt="">
@@ -49,19 +49,19 @@ define(['jquery','jqueryForm','login'],function($){
                                     </div>
                                     <button class="detail">查看详情</button>
                                 </div>
-                            </div>`
+                            </div>`;
 
                 var choice_option = {
-                    url:"chooseRoom.action",
-                    type:"POST",
-                    dataType:"json",
-                    success:function(data){
+                    url: "chooseRoom.action",
+                    type: "POST",
+                    dataType: "json",
+                    success: function (data) {
                         var data = $.parseJSON(data.content);
                         var length = data.length;
                         $(".hotel-list").empty();
                         //console.log(data.length);
                         $("#result_num").text(length);
-                        for(var i = 0;i < length;i++){
+                        for (var i = 0; i < length; i++) {
                             $(".hotel-list").append(model);
                             $(".address").eq(i).text(data[i].direction);
                             $(".price_num").eq(i).text(data[i].price);
@@ -69,23 +69,23 @@ define(['jquery','jqueryForm','login'],function($){
                             $(".score_num").eq(i).text(data[i].score);
                             $(".comment_num").eq(i).text(data[i].comment);
                             $(".spare").eq(i).text(data[i].sparelive);
-                        } 
+                        }
                     }
                 };
 
                 $.ajax({
-                    url:'/Hotel/WebApp/Order-header.html',
-                    type:"POST",
-                    dataType:"html",
-                    success:function(data){
+                    url: '/Hotel/WebApp/Order-header.html',
+                    type: "POST",
+                    dataType: "html",
+                    success: function (data) {
                         $(".bodyer").before(data);
                     }
                 });
                 $.ajax({
-                    url:'/Hotel/WebApp/Order-footer.html',
-                    type:"POST",
-                    dataType:"html",
-                    success:function(data){
+                    url: '/Hotel/WebApp/Order-footer.html',
+                    type: "POST",
+                    dataType: "html",
+                    success: function (data) {
                         $(".bodyer").after(data);
                     }
                 });
@@ -96,172 +96,155 @@ define(['jquery','jqueryForm','login'],function($){
                     "type": 'POST',
                     "dataType": 'json',
                     "data": {},
-                    "success":function(data){
+                    "success": function (data) {
                         var data = $.parseJSON(data.content);
-                        if(data[0].status=="1"){
+                        if (data[0].status == "1") {
                             console.log(data[1].username);
                             $('.register').remove();
                             $('.login').remove();
-                            $('.logo-bar').after("<li class='user'><a href='#'>"+data[1].username+"</a></li>");
+                            $('.logo-bar').after("<li class='user'><a href='#'>" + data[1].username + "</a></li>");
                             $(".logo-bar").after("<li class='logout'><a href='#'>退出</a></li>");
                         }
                     },
-                    "error":function(){
+                    "error": function () {
                         console.log("网络错误！");
                     }
                 });
 
-
                 //input[type:checkbox]的唯一选项判定
-                $("input[type=checkbox]").on('click',function(){
-                    $(this).parent().siblings("span").find("input").prop("checked",false);
+                $("input[type=checkbox]").on('click', function () {
+                    $(this).parent().siblings("span").find("input").prop("checked", false);
                     var $name = $(this).prop("id");
                 });
 
-
                 //朝北不允许有双人间
-                $("#north").on("click",function(){
+                $("#north").on("click", function () {
                     $("#ultra").prop({
-                        "disabled":false
-                    }).siblings("label").css("color","#000");
-                    if($(this).prop("checked") == true){
-                        $("#double").prop("disabled",true).siblings().css("color","#999");
-                    }
-                    else {
-                        $("#double").prop("disabled",false).siblings().css("color","#000");
+                        "disabled": false
+                    }).siblings("label").css("color", "#000");
+                    if ($(this).prop("checked") == true) {
+                        $("#double").prop("disabled", true).siblings().css("color", "#999");
+                    } else {
+                        $("#double").prop("disabled", false).siblings().css("color", "#000");
                     }
                 });
 
                 //朝南不允许有四人间
-                $("#south").on("click",function(){
+                $("#south").on("click", function () {
                     $("#double").prop({
-                        "disabled":false
-                    }).siblings("label").css("color","#000");
-                    if($(this).prop("checked") == true){
-                        $("#ultra").prop("disabled",true).siblings().css("color","#999");
-                    }
-                    else {
-                        $("#ultra").prop("disabled",false).siblings().css("color","#000");
+                        "disabled": false
+                    }).siblings("label").css("color", "#000");
+                    if ($(this).prop("checked") == true) {
+                        $("#ultra").prop("disabled", true).siblings().css("color", "#999");
+                    } else {
+                        $("#ultra").prop("disabled", false).siblings().css("color", "#000");
                     }
                 });
 
                 //双人间不允许朝北
-                $("#double").on("click",function(){
+                $("#double").on("click", function () {
                     $("#south").prop({
-                        "disabled":false
-                    }).siblings().css("color","#000");
-                    if($(this).prop("checked") == true){
-                        $("#north").prop("disabled",true).siblings().css("color","#999");
-                    }
-                    else {
-                        $("#north").prop("disabled",false).siblings().css("color","#000");
+                        "disabled": false
+                    }).siblings().css("color", "#000");
+                    if ($(this).prop("checked") == true) {
+                        $("#north").prop("disabled", true).siblings().css("color", "#999");
+                    } else {
+                        $("#north").prop("disabled", false).siblings().css("color", "#000");
                     }
                 });
 
                 //四人间不允许朝南
-                $("#ultra").on("click",function(){
+                $("#ultra").on("click", function () {
                     $("#north").prop({
-                        "disabled":false
-                    }).siblings().css("color","#000");
-                    if($(this).prop("checked") == true){
-                        $("#south").prop("disabled",true).siblings().css("color","#999");
-                    }
-                    else {
-                        $("#south").prop("disabled",false).siblings().css("color","#000");
+                        "disabled": false
+                    }).siblings().css("color", "#000");
+                    if ($(this).prop("checked") == true) {
+                        $("#south").prop("disabled", true).siblings().css("color", "#999");
+                    } else {
+                        $("#south").prop("disabled", false).siblings().css("color", "#000");
                     }
                 });
 
                 //三人间朝向无要求
-                $("#triple").on("click",function(){
+                $("#triple").on("click", function () {
                     $(".choice").eq(0).find("input").prop({
-                        "disabled":false
-                    }).siblings().css("color","#000");
+                        "disabled": false
+                    }).siblings().css("color", "#000");
                 });
 
                 //清除选项
-                $(".clean").on("click",function(){
+                $(".clean").on("click", function () {
                     $(this).siblings(".lab").find("input").prop({
-                        "disabled":false,
-                        "checked":false
+                        "disabled": false,
+                        "checked": false
                     }).find("label").css({
-                        "color":"#000"
-                    })
+                        "color": "#000"
+                    });
                 });
 
-                $(".hotel-list").on('mouseover','.hotel',function(){
+                $(".hotel-list").on('mouseover', '.hotel', function () {
                     $(this).addClass("hotel-hover").find(".detail").addClass("detail-hover");
                 });
-                $(".hotel-list").on('mouseout','.hotel',function(){
+                $(".hotel-list").on('mouseout', '.hotel', function () {
                     $(this).removeClass("hotel-hover").find(".detail").removeClass("detail-hover");
                 });
 
                 //默认排序
-                $("#default_sort").on('click', function() {
+                $("#default_sort").on('click', function () {
                     $(this).siblings("li").removeClass("active");
                     $(this).addClass("active");
                     $.ajax({
-                        type:"POST",
-                        url:"",
-                        data:{"type":"defaultSort"},
-                        dataType:"json",
-                        success:function(data){
-
-                        }
-                    })
+                        type: "POST",
+                        url: "",
+                        data: { "type": "defaultSort" },
+                        dataType: "json",
+                        success: function (data) {}
+                    });
                 });
 
                 //价格低到高排序
-                $("#price_low_hight").on('click', function() {
+                $("#price_low_hight").on('click', function () {
                     $(this).siblings("li").removeClass("active");
                     $(this).addClass("active");
                     $.ajax({
-                        type:"POST",
-                        url:"",
-                        data:{"type":"priceLowHight"},
-                        dataType:"json",
-                        success:function(data){
-
-                        }
-                    })
+                        type: "POST",
+                        url: "",
+                        data: { "type": "priceLowHight" },
+                        dataType: "json",
+                        success: function (data) {}
+                    });
                 });
 
                 //评分低到高排序
-                $("#score_low_height").on('click', function() {
+                $("#score_low_height").on('click', function () {
                     $(this).siblings("li").removeClass("active");
                     $(this).addClass("active");
                     $.ajax({
-                        type:"POST",
-                        url:"",
-                        data:{"type":"scoreLowHight"},
-                        dataType:"json",
-                        success:function(data){
-
-                        }
-                    })
+                        type: "POST",
+                        url: "",
+                        data: { "type": "scoreLowHight" },
+                        dataType: "json",
+                        success: function (data) {}
+                    });
                 });
 
                 //选项及时响应
-                $("#choice_form").on('click', "input", function(event) {
-                   $("#choice_form").submit();
+                $("#choice_form").on('click', "input", function (event) {
+                    $("#choice_form").submit();
                 });
 
-                setTimeout(function(){
+                setTimeout(function () {
                     $("#choice_form").trigger('submit');
-                },1);
-
+                }, 1);
 
                 //阻止表单提交
-                $('#choice_form').on('submit', function(event) {
+                $('#choice_form').on('submit', function (event) {
                     event.preventDefault();
-                    $(this).ajaxSubmit(choice_option)
+                    $(this).ajaxSubmit(choice_option);
                 });
-             });
+            });
         }
-    }
-})
+    };
+});
 
-
-
-
-
-
+//# sourceMappingURL=Order-hotel-compiled.js.map
--- WebContent/WebApp/js/login-compiled.js
+++ WebContent/WebApp/js/login-compiled.js
@@ -1,193 +1,186 @@
-define(['jquery'],function($){
-//退出登录事件
-    $(document).on('click',".logout", function(event) {
+define(['jquery'], function ($) {
+    //退出登录事件
+    $(document).on('click', ".logout", function (event) {
         event.preventDefault();
         $.ajax({
             url: '/Hotel/loginOut.action',
             type: 'GET',
             dataType: 'json',
             data: {},
-            success:function(){
+            success: function () {
                 location.reload();
             }
-        })
+        });
     });
 
-
-//点击关闭按钮事件
-    $(document).on('click', '.close',function() {
+    //点击关闭按钮事件
+    $(document).on('click', '.close', function () {
         $('.hidden').css({
-            "visibility":"hidden"
+            "visibility": "hidden"
         });
         $('.box').removeClass('box-login-active').removeClass('box-register-active');
         $('.box input').val('');
         $('.error').text('');
     });
 
-//点击登录按钮事件
-    $(document).on('click','.login-button',login);
-    function login (){
+    //点击登录按钮事件
+    $(document).on('click', '.login-button', login);
+    function login() {
         console.log(1);
         $('.hidden').css({
-            "visibility":"visible"
+            "visibility": "visible"
         });
         $('.box').addClass('box-login-active').removeClass('box-register-active');
         $('.box-hotel-register').css({
-            "font-size":".16rem",
-            "color":"#7f1f59",
-            "margin-top":".24rem"
+            "font-size": ".16rem",
+            "color": "#7f1f59",
+            "margin-top": ".24rem"
         });
         $('.box-hotel-login').css({
-            "font-size":".2rem",
-            "color":"#000",
-            "margin-top":".21rem"
+            "font-size": ".2rem",
+            "color": "#000",
+            "margin-top": ".21rem"
         });
         $('.login-box').css({
-            "display":"block"
+            "display": "block"
         });
         $('.register-box').css({
-            "display":"none"
+            "display": "none"
         });
     }
 
-//点击注册按钮事件
-    $(document).on('click','.register-button', register);
-    function register(){
+    //点击注册按钮事件
+    $(document).on('click', '.register-button', register);
+    function register() {
         $('.hidden').css({
-            "visibility":"visible"
+            "visibility": "visible"
         });
         $('.box').addClass('box-register-active').removeClass('box-login-active');
         $('.box-hotel-login').css({
-            "font-size":".16rem",
-            "color":"#7f1f59",
-            "margin-top":".24rem"
+            "font-size": ".16rem",
+            "color": "#7f1f59",
+            "margin-top": ".24rem"
         });
         $('.box-hotel-register').css({
-            "font-size":".2rem",
-            "color":"#000",
-            "margin-top":".21rem"
+            "font-size": ".2rem",
+            "color": "#000",
+            "margin-top": ".21rem"
         });
         $('.register-box').css({
-            "display":"block"
+            "display": "block"
         });
         $('.login-box').css({
-            "display":"none"
+            "display": "none"
         });
     }
 
-//登录事件
-    $(document).on('click','#box_up',function() {
+    //登录事件
+    $(document).on('click', '#box_up', function () {
         var $username = encodeURI($("#username").val());
         var $password = $("#password").val();
         $.ajax({
-            "type":"POST",
-            "url":"/Hotel/login.action",
-            "dataType":"json",
-            "data":{"username":$username,"password":$password},
-            "contentType":"application/x-www-form-urlencoded; charset=UTF-8",
-            "success":function(data){
+            "type": "POST",
+            "url": "/Hotel/login.action",
+            "dataType": "json",
+            "data": { "username": $username, "password": $password },
+            "contentType": "application/x-www-form-urlencoded; charset=UTF-8",
+            "success": function (data) {
                 var data = $.parseJSON(data.content);
-                if(data[0].status=="1"){
+                if (data[0].status == "1") {
                     $('#box_up').before('<p class="login-success">登陆成功！</p>');
-                    setTimeout(function(){
+                    setTimeout(function () {
                         location.reload();
-                    },500)
-                }
-                else{
+                    }, 500);
+                } else {
                     $('#box_up').before('<p class="login-error">用户名或者密码错误！</p>');
                 }
             },
-            "error":function(){
+            "error": function () {
                 alert("网络不畅，请稍后再试！");
             }
-        })
+        });
     });
 
-//注册事件
-    $(document).on('click','#box_up_r', function() {
+    //注册事件
+    $(document).on('click', '#box_up_r', function () {
         $.ajax({
-            "type":"GET",
-            "url":"/Hotel/register.action",
-            "dataType":"json",
-            "contentType":"charset=UTF-8",
-            "data":{"username":$register_username,"password":$register_password},
-            "beforeSend":function(){
-                if($('.error').text()!=""||$register_username==""||$register_password==""||$register_password_confirm==""){
+            "type": "GET",
+            "url": "/Hotel/register.action",
+            "dataType": "json",
+            "contentType": "charset=UTF-8",
+            "data": { "username": $register_username, "password": $register_password },
+            "beforeSend": function () {
+                if ($('.error').text() != "" || $register_username == "" || $register_password == "" || $register_password_confirm == "") {
                     return false;
                 }
             },
-            "success":function(data){
+            "success": function (data) {
                 var data = $.parseJSON(data.content);
                 console.log(data[0].username);
                 $('#box_up_r').before('<p class="login-success">注册成功！</p>');
-                setTimeout(function(){
+                setTimeout(function () {
                     location.reload();
-                },500)
+                }, 500);
             },
-            "error":function(){
+            "error": function () {
                 alert("网络不畅，请稍后再试！");
             }
-        })
+        });
     });
 
-//检测用户名是否被占用和是否符合规范
-    $(document).on('blur','#register_username',  function(event) {
+    //检测用户名是否被占用和是否符合规范
+    $(document).on('blur', '#register_username', function (event) {
         event.preventDefault();
         var $username = $(this).val();
-        if($username==""){
+        if ($username == "") {
             $('.error-username').text('用户名不能为空');
-        }
-        else if(!$reg1.test($username)) {
+        } else if (!$reg1.test($username)) {
             $('.error-username').text('用户名不合规范');
-        }
-        else{
+        } else {
             $register_username = encodeURI($username);
             $('.error-username').text('');
             $.ajax({
                 "url": '/Hotel/check.action',
                 "type": 'GET',
                 "dataType": 'json',
-                "data": {"username": $register_username},
-                "success":function(data){
+                "data": { "username": $register_username },
+                "success": function (data) {
                     var data = $.parseJSON(data.content);
-                    if(data[0].status=="0"){
+                    if (data[0].status == "0") {
                         $('.error-username').text('用户名已被占用');
-                    }
-                    else if(data[0].status=="1"){
+                    } else if (data[0].status == "1") {
                         $('.error-username').text('');
                     }
                 }
-            })
+            });
         }
     });
 
-//检测密码是否符合规范
-    $(document).on('blur', '#register_password',function(event) {
+    //检测密码是否符合规范
+    $(document).on('blur', '#register_password', function (event) {
         event.preventDefault();
         var $password = $(this).val();
-        if($password==""){
+        if ($password == "") {
             $('.error-password').text('密码不能为空');
-        }
-        else if(!$reg2.test($password)){
+        } else if (!$reg2.test($password)) {
             $('.error-password').text('密码不合规范');
-        }
-        else{
+        } else {
             $('.error-password').text('');
             $register_password = $password;
         }
     });
 
-//检测两次密码输入是否一致
-    $(document).on('blur','#register_password_confirm', function(event) {
+    //检测两次密码输入是否一致
+    $(document).on('blur', '#register_password_confirm', function (event) {
         event.preventDefault();
         var $password = $(this).val();
-        if($register_password_confirm!=$register_password){
+        if ($register_password_confirm != $register_password) {
             $('.error-password-confirm').text('两个密码不一致');
-        }
-        else{
+        } else {
             $('.error-password-confirm').text('');
             $register_password_confirm = $password;
         }
     });
-})
+});
 
+//# sourceMappingURL=login-compiled.js.map
